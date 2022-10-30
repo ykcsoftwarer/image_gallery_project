85c62d92e9b1eac1e8e75a632bebfb05739b7168
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-31 02:00:42 +0300

    done

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -4,13 +4,11 @@ const Picture = (props) => {
   const { photographer, large } = props;
 
   return (
-    <div className="pictures">
-      <div className="imageContainer">
-        <div className="pictures">
-          <div className="picture">
-            <img className="picture" src={large} />
-           <p className="info" >{photographer}</p>
-          </div>
+    <div className="App pictures imageContainer">
+      <div className="morePicture">
+        <div className="card picture">
+          <img className="img picture" src={large} />
+          <p className="info">{photographer}</p>
         </div>
       </div>
     </div>
